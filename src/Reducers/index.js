142fc9e64f9d3ef6e482b2f7d2a81997389b71bb
RohitKumarPattanayak <rohitkumarpattanayak@gmail.com>
2023-01-15 00:54:09 +0530

    redux toolkit setup complete

---
+++ src/Reducers/index.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const counterSlice = createSlice({
+  name: "",
+  initialState: initialState,
+  reducers: {},
+});
+
+export const {} = counterSlice.actions;
+
+export default counterSlice.reducer;
